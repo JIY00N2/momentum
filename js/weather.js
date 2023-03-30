04667c32112b78ca81f8f6981c0d6a6db472e17f
JiYoon <angella990825@gmail.com>
2023-03-31 01:49:55 +0900

    remove api key

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-const API_KEY= config.apikey;
+// const API_KEY= config.apikey;
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${"4fbedc1a772bc5c92623a504bf8da1b0"}&units=metric`;
     fetch(url).then(response => response.json()).then(data=>{
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
