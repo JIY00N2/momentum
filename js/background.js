e035f825303f8504aa49085111d5ee31d455f92a
JiYoon <angella990825@gmail.com>
2023-03-31 01:50:59 +0900

    사진 교체

--- js/background.js
+++ js/background.js
@@ -1,7 +1,8 @@
-const images=["balloon.jpg","blue.jpg","kitten.jpg","moon.jpg","puppy.jpg"];
+const images=["balloon.jpg","darkskies.jpg","star.jpg","moon.jpg","halfmoon.jpg"];
 const chosenImage=images[Math.floor(Math.random()*images.length)];
 
 const bgImage= document.createElement("img");
 bgImage.src=`img/${chosenImage}`;
 // body에 추가
+bgImage.classList.add("bgImage");
 document.body.appendChild(bgImage);
