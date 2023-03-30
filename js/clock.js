496743d43dbc2f39f5a566de11a3abc5cb62d05f
JiYoon <angella990825@gmail.com>
2023-03-31 01:50:49 +0900

    현재 날짜와 현재 시간 추가

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,17 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector(".clock");
+const day = document.querySelector(".date");
+
 
 // 시계 만들기
 function getClock(){
     const date = new Date();
     // .padStart(2,"0") = "01,02.."
-    const hours = String(date.getHours()).padStart(2,"0");
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText =`${hours}:${minutes}:${seconds}`;
+    // const hours = date.getHours();
+    // const seconds = String(date.getSeconds()).padStart(2,"0");
+    //clock.innerText =`${hours}:${minutes}:${seconds}`;
+    day.innerText = new Intl.DateTimeFormat('ko-KR',{dateStyle:'long'}).format(date);
+    clock.innerText = new Intl.DateTimeFormat('ko-KR',{timeStyle:'medium'}).format(date);
+    
 }
 
 // 시계 바로 호출
